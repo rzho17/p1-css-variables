7952b640eb20a52fd057df19883359a63c0a292d
Richard <richardzehungho@gmail.com>
2025-07-07 22:14:11 -0700

    add light/dark mode toggle example

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const changeBtn = document.querySelector("#change-theme");
+
+changeBtn.addEventListener("click", () => {
+  const theme = document.body.getAttribute("data-theme");
+  console.log(theme);
+
+  if (theme === null) {
+    document.body.setAttribute("data-theme", "dark");
+    changeBtn.innerHTML = "Set Light";
+  } else {
+    document.body.removeAttribute("data-theme");
+    changeBtn.innerHTML = "Set Dark";
+  }
+});
